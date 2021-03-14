fb5b5bf3cddaea5687e7c01ac3f70a13031889b8
MattBrooks95 <xmatthew3x@gmail.com>
2021-03-14 17:10:54 +0900

    saw console logs from clicking buttons -> I had to change the setContentView() in onCreate to use the binding object, unti l then I couldn't see the output

--- app/src/main/java/brooks/SaveableTimers/views/ActiveTimersScreen.kt
+++ app/src/main/java/brooks/SaveableTimers/views/ActiveTimersScreen.kt
@@ -1,12 +1,32 @@
 package brooks.SaveableTimers.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import brooks.SaveableTimers.R
+import brooks.SaveableTimers.databinding.ActiveTimersScreenBinding
+
+private lateinit var binding: ActiveTimersScreenBinding
 
 class ActiveTimersScreen: AppCompatActivity() {
+    private val className: String = "ActiveTimersScreen"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.active_timers_screen)
+        binding = ActiveTimersScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setHandlers()
+    }
+
+    private fun setHandlers() {
+        Log.d(className, "setHandlers")
+        val toCreateTimerScreenButton = binding.navigateCreateTimerButton
+        toCreateTimerScreenButton.setOnClickListener {
+            Log.d(className, "Go to make timer screen")
+        }
+
+        val toSavedTimersScreenButton = binding.navigateSavedTimersButton
+        toSavedTimersScreenButton.setOnClickListener {
+            Log.d(className, "go to saved timers screen")
+        }
     }
 }
