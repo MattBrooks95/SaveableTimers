20bb689edb5a124673849de9f0a71eb6fcba6d85
MattBrooks95 <xmatthew3x@gmail.com>
2021-03-20 15:29:10 +0900

    added my buttons from kotlin! Need to figure out what type to use to represent a duration of time, and I need set the button's margins to be like they were when I was using XML

--- app/src/main/java/brooks/SaveableTimers/views/CreateTimerScreen.kt
+++ app/src/main/java/brooks/SaveableTimers/views/CreateTimerScreen.kt
@@ -3,8 +3,9 @@ package brooks.SaveableTimers.views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import brooks.SaveableTimers.R
+import android.widget.LinearLayout
 import brooks.SaveableTimers.databinding.ActivityCreateTimerScreenBinding
+import com.google.android.material.button.MaterialButton
 
 private lateinit var binding: ActivityCreateTimerScreenBinding
 
@@ -15,6 +16,7 @@ class CreateTimerScreen : AppCompatActivity() {
         setContentView(binding.root)
 
         setHandlers()
+        createDurationButtons()
     }
 
     private fun setHandlers() {
@@ -25,6 +27,34 @@ class CreateTimerScreen : AppCompatActivity() {
         binding.createTimerButton.setOnClickListener {
             val intent = Intent(this, ActiveTimersScreen::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun createDurationButtons() {
+        val buttonContainer = binding.timerDurationButtonsContainer
+        val durationField = binding.durationTextInput
+        val durationText = durationField.text
+        val buttonSettings: Array<Pair<String, Int>> = arrayOf(Pair("+20", 20), Pair("+10", 10), Pair("+5", 10), Pair("+1", 1))
+        buttonSettings.forEach {
+            val displayString = it.first;
+            val increaseValue = it.second;
+
+            val newButton: MaterialButton = MaterialButton(this);
+            newButton.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                 0.25f
+            )
+            newButton.text = displayString
+            newButton.setOnClickListener {
+                if (durationText != null) {
+                    val durationTextString = durationText.toString()
+                    var durationValue: Float = if (durationTextString.isNullOrEmpty()) 0.0f else durationTextString.toFloat()
+                    durationValue += increaseValue
+                    durationField.setText(durationValue.toString())
+                }
+            }
+            buttonContainer.addView(newButton)
         }
     }
 }
