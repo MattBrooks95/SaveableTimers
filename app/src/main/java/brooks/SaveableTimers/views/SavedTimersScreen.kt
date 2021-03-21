d02a42e3d03332601ce9c094fcfd3cbd2c77d45b
MattBrooks95 <xmatthew3x@gmail.com>
2021-03-21 14:03:58 +0900

    trying to setup coroutines

--- app/src/main/java/brooks/SaveableTimers/views/SavedTimersScreen.kt
+++ app/src/main/java/brooks/SaveableTimers/views/SavedTimersScreen.kt
@@ -3,6 +3,10 @@ package brooks.SaveableTimers.views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.room.Room
+import brooks.SaveableTimers.data.AppDatabase
+import brooks.SaveableTimers.data.SaveableTimer
 
 import brooks.SaveableTimers.databinding.ActivitySavedTimersScreenBinding
 
@@ -10,11 +14,32 @@ private lateinit var binding: ActivitySavedTimersScreenBinding
 
 
 class SavedTimersScreen : AppCompatActivity() {
+    lateinit var db: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySavedTimersScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setHandlers()
+
+        db = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java, "database-name"
+        ).build()
+        setHandlers()
+
+        val timersArray: List<SaveableTimer> = loadTimers()
+        timersArray.forEach {
+            val displayNameView = TextView(this)
+            displayNameView.text = it.displayName
+
+            val durationNameView = TextView(this)
+            val duration = it.duration
+            if (duration !== null) durationNameView.text = duration.toString()
+
+            binding.savedTimersContainer.addView(displayNameView)
+            binding.savedTimersContainer.addView(durationNameView)
+        }
     }
 
     private fun setHandlers() {
@@ -23,5 +48,9 @@ class SavedTimersScreen : AppCompatActivity() {
             val goToActiveTimersScreenIntent = Intent(this, ActiveTimersScreen::class.java)
             startActivity(goToActiveTimersScreenIntent)
         }
+    }
+
+    private fun loadTimers(): List<SaveableTimer> {
+        return db.saveableTimerDao().getAll()
     }
 }
