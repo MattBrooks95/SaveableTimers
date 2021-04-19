073c9e547e1c7f985ffd70e9b48ab03d5503ffd4
MattBrooks95 <xmatthew3x@gmail.com>
2021-04-19 21:14:10 +0900

    the alarm was fired! and it opened an activity!

--- app/src/main/java/brooks/SaveableTimers/androidWrappers/SavedTimerReceiver.kt
+++ app/src/main/java/brooks/SaveableTimers/androidWrappers/SavedTimerReceiver.kt
@@ -3,6 +3,7 @@ package brooks.SaveableTimers.androidWrappers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.util.Log
 import androidx.core.content.ContextCompat.startActivity
 import brooks.SaveableTimers.views.RingingScreen
@@ -13,6 +14,7 @@ class SavedTimerReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         Log.d(className, "the alarm was fired!")
         val launchViewIntent: Intent = Intent(context, RingingScreen::class.java)
+        launchViewIntent.flags = FLAG_ACTIVITY_NEW_TASK
         startActivity(context, launchViewIntent, null)
     }
 }
