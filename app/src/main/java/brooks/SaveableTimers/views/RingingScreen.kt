b00eb0cdd99631f75b30f028aa26ad42b5548a24
MattBrooks95 <xmatthew3x@gmail.com>
2021-06-25 22:23:16 +0900

    prepping for playing a sound when the alarm goes off, trying to harcode the sound file just to test it, but I can't get the path right so maybe just implementing the file picker would  be easier

--- app/src/main/java/brooks/SaveableTimers/views/RingingScreen.kt
+++ app/src/main/java/brooks/SaveableTimers/views/RingingScreen.kt
@@ -1,13 +1,18 @@
 package brooks.SaveableTimers.views
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import brooks.SaveableTimers.data.AppDatabase
 import brooks.SaveableTimers.data.SaveableTimer
 import brooks.SaveableTimers.databinding.RingingViewBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.IOException
 
 private lateinit var binding: RingingViewBinding
 
@@ -15,6 +20,7 @@ val scope = MainScope()
 class RingingScreen : AppCompatActivity() {
     private val className: String = "RingingScreen"
     lateinit var db: AppDatabase
+    private var mediaPlayer: MediaPlayer? = MediaPlayer()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = RingingViewBinding.inflate(layoutInflater)
@@ -33,6 +39,39 @@ class RingingScreen : AppCompatActivity() {
             val savedTimer: SaveableTimer = getSavedTimerFromDb(savedTimerId)
 
             populateTextFields(savedTimer)
+        }
+
+        setHandlers();
+        ring();
+    }
+
+    private fun setHandlers(){
+        binding.dismissButton.setOnClickListener {
+            dismissAlarm();
+        }
+    }
+
+    private fun dismissAlarm() {
+        mediaPlayer?.release()
+        mediaPlayer = null
+        finish();
+    }
+
+    private fun ring() {
+        try {
+            mediaPlayer?.apply {
+                setAudioAttributes(
+                        AudioAttributes.Builder()
+                                .setContentType((AudioAttributes.CONTENT_TYPE_MUSIC))
+                                .setUsage(AudioAttributes.USAGE_ALARM)
+                                .build()
+                )
+                setDataSource(applicationContext, File("Phone/Music/zoinks.mp3").toUri())
+                prepare()
+                start()
+            }
+        } catch (error: IOException) {
+            Log.d("RingingScreen", "starting the media player failed:" + error.message)
         }
     }
 
