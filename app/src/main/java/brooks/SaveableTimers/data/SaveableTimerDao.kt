e90477972dd5b8921a4540a1b6038d7d8162074f
matt <xmatthew3x@gmail.com>
2021-08-15 19:36:59 +0900

    learning some sql! subquery and joining, summing & handling negative values

--- app/src/main/java/brooks/SaveableTimers/data/SaveableTimerDao.kt
+++ app/src/main/java/brooks/SaveableTimers/data/SaveableTimerDao.kt
@@ -7,7 +7,10 @@ interface SaveableTimerDao {
     @Query("SELECT * FROM SaveableTimer")
     suspend fun getAll(): List<SaveableTimer>
 
-    @Query("SELECT st.*, sum(at.currently_active) as sumActive FROM SaveableTimer st JOIN ActiveTimer at ON st.uid=at.uid")
+    //every saveable timer gets returned even if it doesn't have activation records in the ActiveTimers table
+    //those that do have records, will be the sum of those records that are still active, or 0
+    //if they have no entries in the ActiveTimers table at all, then the null is cast to 0
+    @Query("SELECT st.*, ifnull(subq.numActive, 0) sumActive FROM SaveableTimer st LEFT JOIN (SELECT at.uid, sum(at.currently_active) numActive FROM ActiveTimer at GROUP BY at.uid) subq  ON subq.uid=st.uid GROUP BY st.uid")
     suspend fun getAllAndActiveStatus(): List<SaveableTimerWithNumberOfActiveActiveTimerEntries>
 
     @Query("SELECT * from SaveableTimer WHERE uid= :uid")
